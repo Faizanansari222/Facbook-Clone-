9382299cf2c40b5e5baed1af98bef1b38a8cce52
Faizanansari222 <faizanrazaansari07@gmail.com>
2024-01-02 19:04:08 +0500

    FaceBook Clone Start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [fbData, setFbData] = useState([]);
+  function rederAPI() {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((res) => setFbData(res.products));
+    // console.log(res.products);
+  }
+  useEffect(() => {
+    rederAPI();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="main ">
+          <div className="top-content flex justify-center items-center ">
+            <div className="img-icon">
+              <img
+                className="w-10 bg-white rounded-full mr-4"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Cirila-%D0%90-majuskla.svg/800px-Cirila-%D0%90-majuskla.svg.png"
+                alt=""
+              />
+            </div>
+            <div className="flex-col m[-10] ">
+              <h1 className=" text-base font-semibold"> {fbData[0].title} </h1>
+
+              <p className="inline-block text-xs font-light"> Sponsored</p>
+            </div>
+            
+          </div>
+
+          <div className="img-content">
+
+
+
+
+          </div>
+
+          {/* <h1>{item.title}</h1> */}
+        </div>
+        {fbData.map(function (item) {
+          return <></>;
+        })}
       </header>
     </div>
   );
